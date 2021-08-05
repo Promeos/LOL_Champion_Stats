25a04b06f4a78daa7e19a57d064f1aec119943c7
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-08-05 05:04:34 -0500

    Automated data acquisition of Champion stats.

--- src/acquire.py
+++ src/acquire.py
@@ -1,18 +1,39 @@
+import pandas as pd
+import requests
+import json
+import os
 
 
+def load_champion_base_stats():
+    '''
+    League of Legends champion base stats: Patch 11.15
 
+    Parameters
+    ----------
+    None
 
-lol = requests.get('http://ddragon.leagueoflegends.com/cdn/11.15.1/data/en_US/champion.json')
+    Returns
+    -------
+    dataset :  pandas.core.DataFrame
+        A dataframe of champion base stats from League of Legends
+    '''
+    filename = './data/champ_stats.csv'
 
-data = json.loads(lol.text).get('data', None)
-champion_names = list(data.keys())
+    if os.path.exists(filename):
+        return pd.read_csv(filename)
+    else:
+        lol = requests.get('http://ddragon.leagueoflegends.com/cdn/11.15.1/data/en_US/champion.json')
 
-df_stats = pd.DataFrame()
+        stats = json.loads(lol.text).get('data', None)
+        champion_names = list(stats.keys())
+        dataset = pd.DataFrame()
 
-for champion in champion_names:
+        for champion in champion_names:
 
-    champ_stats = pd.Series(data[champion]['stats']).to_frame(name=champion)
-    df_stats = pd.concat([df_stats, champ_stats], axis=1)
+            champ_stats = pd.Series(stats[champion]['stats']).to_frame(name=champion)
+            dataset = pd.concat([dataset, champ_stats], axis=1)
 
-df_stats = df_stats.T
-df_stats.reset_index().rename(columns={'index': 'champion'}, inplace=True)
+        dataset = dataset.T
+        dataset = dataset.reset_index().rename(columns={'index': 'champion'})
+        dataset.to_csv(filename, index=False)
+        return dataset
